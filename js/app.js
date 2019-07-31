709e9b9842371c735a559d33a1215712b9c04434
slib <hxy19901208@gmail.com>
2019-07-31 22:38:19 +0800

    修改

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 ;(function(Vue) {
+	const todos = [
+		{
+			id: 1,
+			title: '吃饭',
+			completed: false
+		},{
+			id: 2,
+			title: '睡觉',
+			completed: true
+		},{
+			id: 3,
+			title: '打豆豆',
+			completed: true
+		}
+	]
+
+
 	new Vue({
-		el: ''
+		el: '#todoapp',
+		data: {
+			todos
+		}
 	})
 })(Vue)
