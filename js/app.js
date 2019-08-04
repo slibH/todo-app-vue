083215098110c7a5c6024468af9d061be27a403b
slib <hxy19901208@gmail.com>
2019-08-04 18:02:27 +0800

    完善添加任务

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-;(function(Vue) {
+; (function (Vue) {
 	const todos = [
 		{
 			id: 1,
 			title: '吃饭',
 			completed: false
-		},{
+		}, {
 			id: 2,
 			title: '睡觉',
 			completed: true
-		},{
+		}, {
 			id: 3,
 			title: '打豆豆',
 			completed: true
@@ -20,6 +20,30 @@
 		el: '#todoapp',
 		data: {
 			todos
+		},
+		methods: {
+			addTodo (event) {
+				var todoText = event.target.value.trim();
+				if (!todoText.length) {
+					return
+				}
+
+				var id = this.todos[this.todos.length - 1].id + 1
+
+				this.todos.push({
+					id: id,
+					title: todoText,
+					completed: false
+				})
+
+				event.target.value = ''
+			},
+
+			toggleAll (event) {
+				var checked = event.target.checked
+
+				this.todos.forEach(todo => todo.completed = checked)
+			}
 		}
 	})
 })(Vue)
